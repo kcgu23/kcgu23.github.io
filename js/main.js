b565e437a4c67776a125c0dea502ce8280027aca
kcg52 <kcg52@cornell.edu>
2020-01-14 16:23:45 -0500

    Added creative writing files, changed sidebar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+var closed = true;
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  closed = false;
 }
 
-/* Set the width of the side navigation to 0 */
+/* Set the width of the side navigation to 17px */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "17px";
+  closed = true;
+}
+
+/* Open or close sidenav after clicked */
+function clickNav() {
+  if (closed) {
+    openNav()
+  } else {
+    closeNav()
+  }
 }
