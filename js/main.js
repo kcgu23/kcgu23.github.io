359632cf8e614a845e38d2f90da6dd249eeb6fd6
Kristina Gu <kristina@astralclocktowerstudios.com>
2020-09-15 17:25:52 -0400

    Update Genetic QWOP in portfolio

--- js/main.js
+++ js/main.js
@@ -20,3 +20,11 @@ function clickNav() {
     closeNav()
   }
 }
+
+/* Video popup */
+$(document).ready(function() {
+  $('#headerVideoLink').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });         
+});
