0e0d2118a25b2482c2cf8d298e7ac32067aa3287
Kristina Gu <kristina@astralclocktowerstudios.com>
2020-10-10 01:28:56 -0400

    Add individual pages for each writing sample

--- js/main.js
+++ js/main.js
@@ -20,27 +20,3 @@ function clickNav() {
     closeNav()
   }
 }
-
-/* Video popup */
-function onVideoClick(theLink) {
-    document.getElementById("video_pop").innerHTML = "<video autoplay muted loop id=\"the_Video\"><source src=\""+theLink+"\" type=\"video/webm\"></video>";
-    document.getElementById("video_pop").style.display="block";
-} 
-
-function onPopClick() {
-    document.getElementById("video_pop").style.display="none";
-    document.getElementById("video_pop").innerHTML = ""; 
-}         
-
-var element = document.getElementById("video_pop");
-
-element.onclick = (function (onclick) {
-  return function(evt) {
-    evt  = evt || event;
-
-    if (onclick) {
-      onclick(evt);
-    }
-     if (evt.target == element) { element.style.display="none"; } 
-  }
-})(element.onclick);
